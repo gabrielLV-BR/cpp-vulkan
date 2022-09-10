b41016a42d7176fed971637a8350bec05ade9ffa
Gabriel L <gabriellovato15@gmail.com>
2022-09-10 20:37:45 -0300

    Script for shader compilation on Windows and small tweak on CMakeLists

--- scripts/compileShaders.ps1
+++ scripts/compileShaders.ps1
@@ -1,1 +1,3 @@
-# Will try to make it someday
+# Made by Enzo Zardo -> https://github.com/EnzoZardo
+# For now, GLSLC must be in path (will be fixed soon)
+Get-ChildItem ".\src\resources\shaders\*.*" -Exclude "*.spv"  | ForEach-Object { glslc $_.FullName -o ($_.FullName + ".spv")}
